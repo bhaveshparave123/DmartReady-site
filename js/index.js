f19148d1e8d5d71f4d1c0105bef422210ad8a0ed
bhaveshparave123 <162880354+bhaveshparave123@users.noreply.github.com>
2024-12-12 23:20:57 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+// let num = prompt("hello");
+
+// if (num % 5 === 0) {
+//     console.log(num, "is multiple of 5")
+// }else {
+//     console.log(num, "is not multiple of 5");
+// }
+
+
+
+// let score = 75;
+// let grade;
+
+// if (score >= 90 && score <= 100 ) {
+//     grade = "A";
+// }
+// else if (score >= 70 && score <= 89 ){
+//     grade = "B";
+// }
+// else if (score >= 50 && score <= 69){
+//     grade = "C";
+// }
+// else if (score >= 0 && score <= 49){
+//     grade = "F";
+// }
+
+// console.log("your grade was", grade)
+
+
+
+// let companies = []
